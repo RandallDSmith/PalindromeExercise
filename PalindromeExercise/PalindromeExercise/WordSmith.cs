2a7bc009beea898834a2a2bc45c717f1a643d2ef
Randall Smith <rdsmith1761@gmail.com>
2023-03-18 10:43:19 -0400

    PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var isPal = "";
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                isPal += word[i];
+            }
+            if(isPal == word)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
+using PalindromeExercise;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("orange", false)]
+        [InlineData("hello", false)]
+        [InlineData("noon", true)]
 
-        }
+            public void PalindromeTester(string input, bool expected)
+            {
+                WordSmith tester = new WordSmith();
+               
+                var actual = tester.IsAPalindrome(input);
+
+                Assert.Equal(expected, actual);
+            }
+
+
+
+        
     }
 }
